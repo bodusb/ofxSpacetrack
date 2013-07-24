b7404dff889db36398477aa6a377f7151c32201c
giugrigoli <griugrigoli@gmail.com>
2013-07-24 17:48:04 -0300

    Update ofxSpacetrack.cpp
    processTLE() - Giulia 24/07

--- src/ofxSpacetrack.cpp
+++ src/ofxSpacetrack.cpp
@@ -22,12 +22,46 @@ YMD ofxSpacetrack::convertJ2000toYMD(double value){
 }
 
 // Process the TLE
+
 bool ofxSpacetrack::processTLE(){
+    double tumin; // minutes in one time unit
+    double mu; //earth gravitational parameter
+    double radiusearthkm; //radius of the earth in km
+    double xke; //reciprocal of tumin
+    double j2;
+    double j3;
+    double j4;
+    double j3oj2; // j3/j2
+    double ro[3]; //position vector
+    double vo[3]; //velocity vector
+    double startmfe, stopmfe, deltamin;
+    File infile;
 
-    if(this->fileTLE != NULL){
+    getgravconst( "wgs72", tumin, mu, radiusearthkm, xke, j2, j3, j4, j3oj2 );
 
+    //input 2-line element set file
+    infile = fopen(this->fileTLE, "r");
+    if (infile == NULL){
+        cout<< "Failed to open file"<<endl;
+        return 1;
     }
 
+    while (feof(infile) == 0)
+    {
+        do
+        {
+            fgets( longstr1,130,infile);
+            strncpy(str, &longstr1[0], 1);
+            str[1] = '\0';
+        }while ((strcmp(str, "#")==0)&&(feof(infile) == 0));
+
+        if (feof(infile) == 0)
+        {
+            fgets( longstr2,130,infile);
+            twoline2rv( longstr1, longstr2, "m", "m", "wgs72", startmfe, stopmfe, deltamin, satrec );
+            sgp4("wgs72", satrec, 0.0, ro, vo); //function of propagation
+        }
+    }
 
 }
 
