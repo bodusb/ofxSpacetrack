62b19f416b2cc2b3ac57b886b8432645bc017337
giugrigoli <griugrigoli@gmail.com>
2013-07-25 15:18:11 -0300

    Create testApp.cpp

---
+++ testApp.cpp
@@ -0,0 +1,62 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    st.setFileTLE("SCD1_2LINE.DAT");
+    st.processTLE();
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    st.Update();
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){
+
+}
